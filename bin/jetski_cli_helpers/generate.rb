d41e60a3f8cf9d0fbca85b717bac7fe253673f81
Enoch Tamulonis <enochtamulonis@gmail.com>
2025-12-04 20:24:17 +0700

    refactor on generate code. Clean up gsub code.

--- bin/jetski_cli_helpers/generate.rb
+++ bin/jetski_cli_helpers/generate.rb
@@ -7,8 +7,11 @@ module JetskiCLIHelpers
       controller_file_path = "app/controllers/#{name}_controller.rb"
       copy_file("controllers/example_controller", controller_file_path)
       gsub_file(controller_file_path, /CONTROLLER_NAME/, name.capitalize)
-      actions.each do |action_name|
-        action_content = "def #{action_name}\nend\nACTION_NAME"
+      actions.each.with_index do |action_name, idx|
+        action_content = "def #{action_name}\n  end"
+        if actions.size < (idx + 1)
+          action_content += "\nACTION_NAME"
+        end
         gsub_file(controller_file_path, /ACTION_NAME/, action_content.strip)
         path_to_view = "app/views/#{name}/#{action_name}.html"
 
@@ -17,8 +20,8 @@ module JetskiCLIHelpers
         gsub_file(path_to_view, /CONTROLLER_NAME/, name)
         gsub_file(path_to_view, /ACTION_NAME/, action_name)
         gsub_file(path_to_view, /PATH_TO_VIEW/, path_to_view)
+        say "🌊 View your new page at http://localhost:8000/#{name}/#{action_name}"
       end
-      gsub_file(controller_file_path, /ACTION_NAME/, '')
     end
         
     def self.source_root
