3a0d2e12b3aae471e9966df296fa45036f480632
Enoch Tamulonis <enochtamulonis@gmail.com>
2025-12-26 11:27:26 +0700

    add in db commands

--- lib/jetski/model.rb
+++ lib/jetski/model.rb
@@ -4,12 +4,27 @@ module Jetski
     class << self
       def create(**args)
         return puts "#{table_name.capitalize}.create was called with no args" if args.size == 0
-        question_mark_args = (1..args.size).map { |n| "?" }.join(", ")
-        db.execute "insert into #{pluralized_table_name} values ( #{question_mark_args} )", args
+        data_values = args.map { |k,v| v }
+        key_names = args.map { |k, v| k }
+
+        sql_command = <<~SQL
+          INSERT INTO #{pluralized_table_name} (#{key_names.join(", ")}) VALUES (#{(1..key_names.size).map { |n| "?" }.join(", ")})
+        SQL
+
+        db.execute(sql_command, data_values)
+      end
+
+      def all
+        db.execute( "select * from #{pluralized_table_name}" )
+      end
+
+      def count
+        all.size
       end
 
       def attributes
-        
+        columns, *rows = db.execute2( "select * from #{pluralized_table_name}" )
+        columns
       end
     private
       def table_name
