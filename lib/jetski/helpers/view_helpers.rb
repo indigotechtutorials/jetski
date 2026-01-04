819fdb4ad6560ee4351fd13f0b0c810744ed9e0d
Enoch Tamulonis <enochtamulonis@gmail.com>
2026-01-04 20:18:47 +0700

    new view helpers lots more

--- lib/jetski/helpers/view_helpers.rb
+++ lib/jetski/helpers/view_helpers.rb
@@ -21,16 +21,32 @@ module Jetski
       end
 
       # button_to("Click me", class: "cool-btn")
-      def button_to(button_text, **html_opts)
-        "<button #{format_html_options(**html_opts)}>#{button_text}</button>"
+      def button_to(button_text, url = nil, **html_opts)
+        if url
+          _method = html_opts[:method] || 'POST'
+          # Create form with button inside lol
+          _form = "<form action='#{url}' method='#{_method}'>"
+          _form += "<button type='submit' #{format_html_options(**html_opts)}>#{button_text}</button>"
+          _form += "</form>"
+          _form
+        else
+          _button = "<button #{format_html_options(**html_opts)}>#{button_text}</button>"
+        end
       end
 
-      def input_tag(**html_opts)
+      def input_tag(input_name = nil, **html_opts)
+        html_opts[:name] ||= input_name
         "<input #{format_html_options(**html_opts)}>"
       end
 
-      def textarea_tag(**html_opts)
-        "<textarea #{format_html_options(**html_opts.except(:value))}>#{html_opts[:value]}</textarea>"
+      def label_tag(text, **html_opts)
+        "<label #{format_html_options(**html_opts)}>#{text}</label>"
+      end
+
+      def textarea_tag(name = nil, value = nil, **html_opts)
+        html_opts[:name] ||= name
+        value ||= html_opts[:value]
+        "<textarea #{format_html_options(**html_opts.except(:value))}>#{value}</textarea>"
       end
 
       def image_tag(image_path, **html_opts)
@@ -55,6 +71,10 @@ module Jetski
 
       def javascript_tag(**html_opts)
         "<script #{format_html_options(**html_opts)}>#{yield}</script>"
+      end
+
+      def truncate(text, limit)
+        text[0..limit]
       end
 
       def format_html_options(**html_opts)
