12a4d33dd85876aa84b329a509a367f0b4009610
Enoch Tamulonis <enochtamulonis@gmail.com>
2026-01-04 17:44:26 +0700

    add in the rest of code for crud MVP

--- lib/jetski/router/host/crud.rb
+++ lib/jetski/router/host/crud.rb
@@ -16,11 +16,31 @@ module Jetski
             # Check request url and determine what url it was trying to access.
             req_path = req.request_uri.path
             custom_route_options = all_server_options.dup
+            # Override attributes needed for rendering correct action /posts -> #index
             custom_route_options.tap do |opts|
               case req_path
               when controller_path # /posts
-                  # Override attributes needed for rendering correct action /posts -> #index
                 opts[:action_name] = "index"
+              when "#{controller_path}/new"
+                opts[:action_name] = "new"
+              else
+                url_param = req_path.split("#{controller_path}/")[-1]
+                case
+                when url_param.match(/\d(\/edit)/)
+                  # Edit page
+                  opts[:action_name] = "edit"
+                when url_param.match(/\d\z/)
+                  # matches only id 5 with no additional characters
+                  # Determine if show/update/destroy from request_method
+                  case req.request_method
+                  when "GET"
+                    opts[:action_name] = "show"
+                  when "PUT"
+                    opts[:action_name] = "update"
+                  when "DELETE"
+                    opts[:action_name] = "destroy"
+                  end
+                end
               end
             end
             @controller_host = Jetski::Router::Host::Controller.new(server, **custom_route_options)
