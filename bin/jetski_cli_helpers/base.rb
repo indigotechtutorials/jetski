adf11ec9e02ddce3a791975002441f075c0cb443
Enoch Tamulonis <enochtamulonis@gmail.com>
2026-01-13 13:08:26 +0700

    use template for generating modelfile big cleanup

--- bin/jetski_cli_helpers/base.rb
+++ bin/jetski_cli_helpers/base.rb
@@ -3,5 +3,9 @@ module JetskiCLIHelpers
   class Base < Thor
     include Jetski::Helpers::Generic, Thor::Actions, JetskiCLIHelpers::SharedMethods,
       Jetski::Database::Base
+    
+    def self.source_root
+      File.join(File.dirname(__FILE__), '..', '..', 'templates')
+    end
   end
 end
--- bin/jetski_cli_helpers/generators/model.rb
+++ bin/jetski_cli_helpers/generators/model.rb
@@ -5,19 +5,10 @@ module JetskiCLIHelpers
         # TODO: Instead of creating table here we should have a db:migrate method
         # this will automatically build and patch db tables. Add/remove columns 
         #db.execute create_table_sql(table_name: name, field_names: field_names)
+        @model_name = name
+        @field_names = field_names.map { |f| f.split(":")[0] }
         model_file_path = "app/models/#{name}.rb"
-        create_file model_file_path
-        action_content = if field_names.any?
-          "attributes #{field_names.map { |f| ":#{f.split(":")[0]}" }.join(", ") }"
-        else
-          ""
-        end
-        model_content = <<~MODEL
-          class #{name.capitalize} < Jetski::Model
-            #{action_content}
-          end
-        MODEL
-        append_to_file model_file_path, model_content
+        template "model/template.rb.erb", model_file_path
       end
     end
   end
